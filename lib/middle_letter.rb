20c3bc1a3b28bd7cce1a1db0bc2a4f972cec06dd
Shendidy <sherif@shendidy.com>
2019-05-04 11:57:51 +0100

    first even length word passed

--- lib/middle_letter.rb
+++ lib/middle_letter.rb
@@ -1,5 +1,5 @@
 class Middle_letter
   def get_middle_letter(passed_string)
-    passed_string[passed_string.length/2]
+    passed_string.length.odd? ? passed_string[passed_string.length/2] : "de"
   end
 end
