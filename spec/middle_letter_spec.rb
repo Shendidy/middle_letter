aba0696ad1cbe79f3e635fdda13ddb8178eacc0c
Shendidy <sherif@shendidy.com>
2019-05-04 11:56:40 +0100

    first even length word test

--- spec/middle_letter_spec.rb
+++ spec/middle_letter_spec.rb
@@ -8,4 +8,12 @@ describe Middle_letter do
   it 'should return "a"' do
     expect(subject.get_middle_letter("cat")).to eq('a')
   end
+
+  it 'should return "d"' do
+    expect(subject.get_middle_letter("student")).to eq('d')
+  end
+
+  it 'should return "de"' do
+    expect(subject.get_middle_letter("students")).to eq('de')
+  end
 end
