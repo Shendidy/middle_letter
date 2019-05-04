08767f3b125dc06c058987f73b45739bbfacea15
Shendidy <sherif@shendidy.com>
2019-05-04 12:00:35 +0100

    final version

--- lib/middle_letter.rb
+++ lib/middle_letter.rb
@@ -1,5 +1,6 @@
 class Middle_letter
   def get_middle_letter(passed_string)
-    passed_string.length.odd? ? passed_string[passed_string.length/2] : "de"
+    mid = passed_string.length / 2
+    passed_string.length.odd? ? passed_string[mid] : passed_string[mid - 1] + passed_string[mid]
   end
 end
