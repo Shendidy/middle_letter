9602ec16d0b4ec8d782f63e7324c875bf868b9a1
Shendidy <sherif@shendidy.com>
2019-05-04 11:54:25 +0100

    second odd word failing test

--- spec/middle_letter_spec.rb
+++ spec/middle_letter_spec.rb
@@ -4,4 +4,8 @@ describe Middle_letter do
   it 'should return "o"' do
     expect(subject.get_middle_letter("dog")).to eq('o')
   end
+
+  it 'should return "a"' do
+    expect(subject.get_middle_letter("cat")).to eq('a')
+  end
 end
